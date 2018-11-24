9bf009151ccaf86a7317140a74882e65f692d623
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2018-11-23 23:41:08 -0800

    flow-ify FloatingButton

--- source/components/FloatingButton/index.js
+++ source/components/FloatingButton/index.js
@@ -1,59 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// @flow
+import * as React from 'react';
 
 import './styles.scss';
+
+type Props = {
+    children: React.Node,
+    /** Additional class name */
+    className?: string,
+    /**
+     * href attribute.
+     * FloatingButton uses `<a>` tag if it's present.
+     */
+    href?: string | false,
+    /** Disabled attribute for the `<button>` */
+    disabled?: bool,
+    /** Callback for the `<button>` */
+    onClick?: Function,
+};
 
 /**
  * Floating button (icons-only)
  */
-const FloatingButton = ({
-    className,
-    href,
-    children,
-    ...rest
-}) => {
-    const classes = [
-        'wds-floating-button',
-        className,
-    ].filter(c => c).join(' ');
-
-    if (href) {
-        return <a href={href} className={classes} {...rest}>{children}</a>;
+export default class FloatingButton extends React.PureComponent<Props> {
+    static defaultProps = {
+        className: '',
+        href: false,
+        disabled: false,
+        onClick: () => {},
     }
 
-    return <button className={classes} {...rest}>{children}</button>;
-};
+    render() {
+        const {
+            className,
+            children,
+            href,
+            ...rest
+        } = this.props;
 
-FloatingButton.propTypes = {
-    /**
-   * @ignore
-   */
-    children: PropTypes.node,
-    /**
-   * href attribute.
-   * FloatingButton uses `<a>` tag if it's present.
-   */
-    className: PropTypes.string,
-    /**
-   * Additional class name
-   */
-    disabled: PropTypes.bool,
-    /**
-   * Disabled attribute for the `<button>`
-   */
-    href: PropTypes.string,
-    /**
-   * Callback for the `<button>`
-   */
-    onClick: PropTypes.func,
-};
+        const classes = [
+            'wds-floating-button',
+            className,
+        ].filter(c => c).join(' ');
 
-FloatingButton.defaultProps = {
-    children: null,
-    className: '',
-    disabled: false,
-    href: null,
-    onClick: () => {},
-};
+        if (href) {
+            return <a href={href} className={classes} {...rest}>{children}</a>;
+        }
 
-export default FloatingButton;
+        return <button className={classes} {...rest}>{children}</button>;
+    }
+}
